37405545eb6ac2f40985ac5c2f6374c77843e45e
ruolin shen <ruolinshen@MB081849.local>
2020-08-13 17:29:34 +0200

    finishing writing training and testing function, run test in jupyter notebook but all functions are all in the deepregression.py

--- deepregression.py
+++ deepregression.py
@@ -1,7 +1,12 @@
-
+import torch
 from torch import nn
 import numpy as np
+from torch.utils.data import Dataset, DataLoader
+import torch.optim as optim
+import pandas as pd
 
+
+## SDDR NETWORK PART
 class Sddr_Single(nn.Module):
     
     def __init__(self, deep_models_dict, deep_shapes, struct_shapes, P):
@@ -53,7 +58,7 @@ class Sddr_Single(nn.Module):
             for key in self.deep_models_dict.keys(): #assume that the input for the NN has the name of the NN as key
                 net = self.deep_models_dict[key]
                 Uhat_list.append(net(datadict[key]))
-
+            
             Uhat = torch.cat(Uhat_list, dim = 1) #concatenate the outputs of the deep NNs
 
             Utilde = self._orthog_layer(Q, Uhat)
@@ -107,6 +112,8 @@ class Sddr(nn.Module):
         #define distributional layer
         if family == "normal":
             self.distribution_layer_type = torch.distributions.normal.Normal
+        elif family == "poisson":
+            self.distribution_layer_type = torch.distributions.poisson.Poisson
     
     def _distribution_trafos(self,pred):
         #applies the specific transformations to the prediction so they they correspond to the restrictions
@@ -119,16 +126,19 @@ class Sddr(nn.Module):
         if family == "normal":
             pred_trafo["loc"] = pred["loc"]
             pred_trafo["scale"] = add_const + pred["scale"].exp()
+        elif family == "poisson":
+            pred_trafo["rate"] = add_const + pred["rate"].exp()
         
         return pred_trafo
     
     def forward(self,meta_datadict):
         
+        self.regul = 0
         pred = dict()
         for parameter_name, data_dict  in meta_datadict.items():
-            print(parameter_name)
             sddr_net = self.single_parameter_sddr_list[parameter_name]
             pred[parameter_name] = sddr_net(data_dict)
+            self.regul += sddr_net.get_regularization()*self.regularization_params[parameter_name]
             
         predicted_parameters = self._distribution_trafos(pred)
         
@@ -139,14 +149,91 @@ class Sddr(nn.Module):
     
     def get_loss(self, Y):
     
-        regul = 0
-        for parameter_name, data_dict  in meta_datadict.items():
-            sddr_net = self.single_parameter_sddr_list[parameter_name]
-            regul += sddr_net.get_regularization()*self.regularization_params[parameter_name]
+#         regul = 0            # move to forward, or we need meta_datadict as input to get_loss
+#         for parameter_name, data_dict  in meta_datadict.items():
+#             sddr_net = self.single_parameter_sddr_list[parameter_name]
+#             regul += sddr_net.get_regularization()*self.regularization_params[parameter_name]
         log_loss = -self.distribution_layer.log_prob(Y)
-        loss = log_loss + regul
+        loss = log_loss + self.regul
         
         return loss
         
         
-# missing: train and test
+
+
+        
+        
+## TRAIN and TEST on the small case in example_data/simple_gam/
+
+class MyDataset(Dataset):
+    def __init__(self):
+        x_csv = pd.read_csv (r'./example_data/simple_gam/X.csv',sep=';',header=None)
+        y_csv = pd.read_csv (r'./example_data/simple_gam/Y.csv',header=None)
+        B_csv = pd.read_csv (r'./example_data/simple_gam/B.csv',sep=';',header=None)
+        
+        self.struct_data = torch.from_numpy(B_csv.values).float()
+        self.deep_data = torch.from_numpy(x_csv.values).float()
+        self.y = torch.from_numpy(y_csv.values).float()
+        
+    def __getitem__(self, index):
+        struct = self.struct_data[index]
+        deep = self.deep_data[index]
+        gt = self.y[index]
+        
+        datadict = {"structured": struct, "dm1": deep}
+        meta_datadict = dict()
+        meta_datadict["rate"] = datadict
+        
+        return {'meta_datadict': meta_datadict, 'target': gt}        
+    
+    def __len__(self):
+        return len(self.deep_data)
+
+
+def train():
+    
+    family = "poisson"
+    
+    regularization_params = dict()
+    regularization_params["rate"] = 1.   # already mutiplied in full_P
+    
+    deep_models_dict = {}
+    deep_shapes = {}
+    struct_shapes = 19
+    P = pd.read_csv (r'./example_data/simple_gam/full_P.csv',sep=';',header=None).values
+    
+    parsed_formula_contents = dict()
+    parsed_formula_contents["rate"] = {"deep_models_dict": deep_models_dict, "deep_shapes": deep_shapes, "struct_shapes": struct_shapes, "P": P}
+    
+
+    dataset = MyDataset()
+    loader = DataLoader(
+        dataset,
+        batch_size=1000,
+    )
+    
+    
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    bignet = Sddr(family, regularization_params, parsed_formula_contents)
+    bignet = bignet.to(device)
+    optimizer = optim.RMSprop(bignet.parameters())
+
+    bignet.train()
+    print('Begin training ...')
+    for epoch in range(1, 2500):
+
+        for batch in loader:
+            target = batch['target'].to(device)
+            meta_datadict = batch['meta_datadict']          # .to(device) should be improved 
+            meta_datadict['rate']['structured'] = meta_datadict['rate']['structured'].to(device)
+            meta_datadict['rate']['dm1'] = meta_datadict['rate']['dm1'].to(device)
+           
+            optimizer.zero_grad()
+            output = bignet(meta_datadict)
+            loss = torch.mean(bignet.get_loss(target))
+            loss.backward()
+            optimizer.step()
+        if epoch % 100 == 0:
+            print('Train Epoch: {} \t Loss: {:.6f}'.format(epoch,loss.item()))
+            
+    return list(bignet.parameters())[0].detach().numpy()
