5e4aa99c4bc4b227b681a0a0eb97fc13e2d289de
christina.bukas@helmholtz-muenchen.de <christina.bukas@helmholtz-muenchen.de>
2020-08-20 14:30:57 +0300

    need to figure out linear and smoothing

--- utils.py
+++ utils.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 '''
 formulas = dict()
 
@@ -13,6 +15,7 @@ meta_datadict = parse_formulas(formulas, data)
 
 def parse_formulas(formulas, data):
     meta_datadict = dict()
+    struct_list = []
     for param in formulas.keys():
         formula = formulas[param]
         # check formula formulation!
@@ -22,21 +25,31 @@ def parse_formulas(formulas, data):
                 # return intercept??
             else:
                 # formula either only has one term OR formula wrongfully written --> HOW to check?
+                if '+' in formula:
+                    print('Rewrite your formula and make sure to leave a space before and after the plus sign')
+                else:
+                    # do the same as in for loop below - turn into function?
         else:
             formula_split = formula.split(' + ')
             for foruma_part in formula_split:
                 if formula_part[0] == 's':
                     # perform smoothing
-                    sx1 = s[data['x1']]
-                    sx2 = s[data['x2']]
+                    input_data_name = formula_part[0].split('(')[-1].split(')')[0]
+                    smooth_data = smoothing[data[input_data_name]]
+                    # extend later to get penalty function
+                    struct_list.append(smooth_data)
                 # unstructured part    
                 elif formula_part[0] == 'd':
                     model_name = formula_part[0].split('(')[0]
                     input_data = formula_part[0].split('(')[-1].split(')')[0]
-                    meta_datadict[param][model_name] = data[input_data]
+                    meta_datadict[param][model_name] = data[input_data_name]
                 # linear part
                 else:
                     l = ..
+                    # first row of matrix should be the linear part
+                    struct_list.append(l)
             # concatenate l,sx1,sx2 and put matrix into
-            meta_datadict[param]['structured'] = struct_matrix
+            meta_datadict[param]['structured'] = np.concatenate(struct_list, dim=1)
             return meta_datadict
+
+
