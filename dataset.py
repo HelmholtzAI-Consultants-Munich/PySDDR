e42b990b48edb90e80941b8eefd91d68fc46cd43
ruolin shen <ruolinshen@MB081849.local>
2020-09-10 17:28:36 +0200

    modify Mydataset to load batch data

--- dataset.py
+++ dataset.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import torch
 from torch.utils.data import Dataset
+from utils import parse_formulas
 
 '''
 x_path = r'./example_data/simple_gam/X.csv'
@@ -8,27 +9,31 @@ y_path = r'./example_data/simple_gam/Y.csv'
 b_path = r'./example_data/simple_gam/B.csv'
 '''
 class MyDataset(Dataset):
-    def __init__(self, x_path, y_path, b_path):
-        x_csv = pd.read_csv (x_path, sep=';',header=None)
-        y_csv = pd.read_csv (y_path, header=None)
-        B_csv = pd.read_csv (b_path, sep=';',header=None).values
+    def __init__(self, x_path, y_path,family, formulas,cur_distribution, deep_models_dict,deep_shapes):
+        
+        x_csv = pd.read_csv(x_path, names=['x1','x2'],delimiter=';')
+        y_csv = pd.read_csv(y_path, header=None)
+        
+        self.parsed_formula_content, self.meta_datadict = parse_formulas(family, formulas, x_csv, cur_distribution, deep_models_dict, deep_shapes)        
+  
+        for param in self.meta_datadict.keys():
+            for key in self.meta_datadict[param].keys():
+                self.meta_datadict[param][key] = torch.from_numpy(self.meta_datadict[param][key]).float()
 
-        #just for now [:,1:]
-        #B_csv = B_csv[:,1:]
-        self.struct_data = torch.from_numpy(B_csv).float()
-        self.deep_data = torch.from_numpy(x_csv.values).float()
         self.y = torch.from_numpy(y_csv.values).float()
         
-    def __getitem__(self, index):
-        struct = self.struct_data[index]
-        deep = self.deep_data[index]
+    def get_parsed_formula_content(self):        
+        return self.parsed_formula_content
+        
+    def __getitem__(self,index):
+        batch_data = {}
+        for param in self.meta_datadict.keys():
+            batch_data[param]={}
+            for key in self.meta_datadict[param].keys():
+                batch_data[param][key] = self.meta_datadict[param][key][index]
+        
         gt = self.y[index]
-        
-        datadict = {"structured": struct, "dm1": deep}
-        meta_datadict = dict()
-        meta_datadict["rate"] = datadict
-        
-        return {'meta_datadict': meta_datadict, 'target': gt}        
+        return {'meta_datadict': batch_data, 'target': gt}        
     
     def __len__(self):
-        return len(self.deep_data)
+        return len(self.y)
--- train.py
+++ train.py
@@ -1,31 +1,47 @@
 
 ## TRAIN and TEST on the small case in example_data/simple_gam
+import pandas as pd
+import torch
+from torch import nn
+import numpy as np
+from torch.utils.data import DataLoader
+import torch.optim as optim
+from deepregression import Sddr_Single, Sddr
+from dataset import MyDataset
+from utils import parse_formulas
 
 def train():
     
-    family = "poisson"
+    cur_distribution = "poisson"
+    family = {'normal':{'loc': 'whateva', 'scale': 'whateva2'}, 'poisson': {'rate': 'whateva'}, 'binomial':{'n': 'whateva', 'p': 'whateva'}}
     
     regularization_params = dict()
     regularization_params["rate"] = 1.   # already mutiplied in full_P
+        
+    x_path = r'./example_data/simple_gam/X.csv'
+    y_path = r'./example_data/simple_gam/Y.csv'
     
-    deep_models_dict = {}
-    deep_shapes = {}
-    struct_shapes = 19
-    P = pd.read_csv (r'./example_data/simple_gam/full_P.csv',sep=';',header=None).values
+    formulas = dict()
+    formulas['rate'] = '~1+bs(x1, df=9)+d1(x1)+d2(x2)'
     
-    parsed_formula_contents = dict()
-    parsed_formula_contents["rate"] = {"deep_models_dict": deep_models_dict, "deep_shapes": deep_shapes, "struct_shapes": struct_shapes, "P": P}
+    deep_models_dict = dict()
+    deep_models_dict['rate'] = dict()
+    deep_models_dict['rate']['d1'] = nn.Sequential(nn.Linear(1,10))
+    deep_models_dict['rate']['d2'] = nn.Sequential(nn.Linear(1,3),nn.ReLU(), nn.Linear(3,8))
     
-
-    dataset = MyDataset()
+    deep_shapes = dict()
+    deep_shapes['rate'] = {"d1" : 10, "d2" : 8}
+    
+    
+    dataset = MyDataset(x_path, y_path,family, formulas,cur_distribution, deep_models_dict,deep_shapes)
     loader = DataLoader(
         dataset,
         batch_size=1000,
     )
-    
+    parsed_formula_contents = dataset.get_parsed_formula_content()
     
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    bignet = Sddr(family, regularization_params, parsed_formula_contents)
+    bignet = Sddr(cur_distribution, regularization_params, parsed_formula_contents)
     bignet = bignet.to(device)
     optimizer = optim.RMSprop(bignet.parameters())
 
@@ -37,7 +53,7 @@ def train():
             target = batch['target'].to(device)
             meta_datadict = batch['meta_datadict']          # .to(device) should be improved 
             meta_datadict['rate']['structured'] = meta_datadict['rate']['structured'].to(device)
-            meta_datadict['rate']['dm1'] = meta_datadict['rate']['dm1'].to(device)
+            meta_datadict['rate']['d1'] = meta_datadict['rate']['d1'].to(device)
            
             optimizer.zero_grad()
             output = bignet(meta_datadict)
