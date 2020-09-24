04b4e6baa97ce24502658f59460d049a9f722e4d
ruolin shen <ruolinshen@MB081849.local>
2020-09-24 15:40:41 +0200

    add family class

--- train.py
+++ train.py
@@ -8,43 +8,43 @@ from torch.utils.data import DataLoader
 import torch.optim as optim
 from deepregression import SddrNet, Sddr_Param_Net
 from dataset import SddrDataset
-from utils import parse_formulas
+from utils import parse_formulas, create_family
 
 class SDDR(object):
     def __init__(self, config):
         self.config = config
-        self.family = # init family
+        family = self.config['current_distribution']
+        self.family_class = create_family(family)
         dataset = SddrDataset(self.config['data_path'], 
                             self.config['ground_truth_path'],
-                            self.family,
+                            self.family_class,
                             self.config['formulas'],
-                            self.config['cur_distribution'],
                             self.config['deep_models_dict'],
                             self.config['deep_shapes'])
 
-        self.regularization_params = self.config['regularization_params']
+        self.regularization_params = self.config['train_parameters']['regularization_params']
 
         self.parsed_formula_contents = dataset.get_parsed_formula_content()
 
         self.loader = DataLoader(dataset,
-                                batch_size=self.config['batch_size'])
+                                batch_size=self.config['train_parameters']['batch_size'])
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-        self.net = SddrNet(cur_distribution, regularization_params, self.parsed_formula_contents)
-        self.net = self.net.to(device)
-        self.optimizer = optim.RMSprop(bignet.parameters())
+        self.net = SddrNet(self.family_class, self.regularization_params, self.parsed_formula_contents)
+        self.net = self.net.to(self.device)
+        self.optimizer = optim.RMSprop(self.net.parameters())
         
-    def train():
+    def train(self):
 
         self.net.train()
         print('Begin training ...')
-        for epoch in range(self.config['epochs']):
+        for epoch in range(self.config['train_parameters']['epochs']):
 
             for batch in self.loader:
-                target = batch['target'].to(device)
+                target = batch['target'].to(self.device)
                 meta_datadict = batch['meta_datadict']          # .to(device) should be improved 
-                meta_datadict['rate']['structured'] = meta_datadict['rate']['structured'].to(device)
-                meta_datadict['rate']['d1'] = meta_datadict['rate']['d1'].to(device)
+                meta_datadict['rate']['structured'] = meta_datadict['rate']['structured'].to(self.device)
+                meta_datadict['rate']['d1'] = meta_datadict['rate']['d1'].to(self.device)
             
                 self.optimizer.zero_grad()
                 output = self.net(meta_datadict)
