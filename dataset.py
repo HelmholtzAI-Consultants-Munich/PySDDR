cd9d892737eb459b6086ff3835505e727bf3e911
Lisa Barros <lisa.barros@helmholtz-muenchen.de>
2020-10-08 11:04:05 +0200

    add dataloader for pandas object input

--- dataset.py
+++ dataset.py
@@ -33,3 +33,33 @@ class SddrDataset(Dataset):
     
     def __len__(self):
         return len(self.y)
+
+
+class SddrDataset_PandasInput(Dataset):
+    def __init__(self, data, target_column, family, formulas, deep_models_dict):
+        
+        
+        #self.firstCov = x_csv.values[:,0]
+        self.parsed_formula_content, self.meta_datadict, self.dm_info_dict = parse_formulas(family, formulas, data, deep_models_dict)        
+
+        for param in self.meta_datadict.keys():
+            for data_part in self.meta_datadict[param].keys():
+                self.meta_datadict[param][data_part] = torch.from_numpy(self.meta_datadict[param][data_part]).float()
+
+        self.y = torch.from_numpy(data[target_column]).float()
+        
+    def get_parsed_formula_content(self):        
+        return self.parsed_formula_content
+        
+    def __getitem__(self,index):
+        batch_data = {}
+        for param in self.meta_datadict.keys():
+            batch_data[param]={}
+            for key in self.meta_datadict[param].keys():
+                batch_data[param][key] = self.meta_datadict[param][key][index]
+        
+        gt = self.y[index]
+        return {'meta_datadict': batch_data, 'target': gt}        
+    
+    def __len__(self):
+        return len(self.y)
