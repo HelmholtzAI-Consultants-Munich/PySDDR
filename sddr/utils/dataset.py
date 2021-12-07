32403adaafc49e0a4036a6caaa71723cb60524ed
felixGer <39033536+felixGer@users.noreply.github.com>
2021-12-07 16:38:15 +0100

    Update dataset.py

--- sddr/utils/dataset.py
+++ sddr/utils/dataset.py
@@ -161,21 +161,21 @@ class SddrDataset(Dataset):
                             else:
                                 images = []
                                 for image_file_name in data_row[cur_feature]:
-                                    #images.append(self.load_csv(root_path, image_file_name))
-                                    images.append(self.load_csv(root_path, image_file_name).unsqueeze(0))
+                                    images.append(self.load_csv(root_path, image_file_name))
+                                    #images.append(self.load_csv(root_path, image_file_name).unsqueeze(0))
                                     
                                 ## pad pack sequences:
-                                #data = torch.nn.utils.rnn.pad_sequence(images, batch_first=True, padding_value=-1.0)
+                                data = torch.nn.utils.rnn.pad_sequence(images, batch_first=True, padding_value=-1.0)
                                 
                                 #data_len = torch.LongTensor(list(map(len, images)))
                                 #data_packed = torch.nn.utils.rnn.pack_padded_sequence(data, data_len, batch_first=True, enforce_sorted=False)
                                     
-                                datadict[param][structured_or_net_name] = torch.cat(images)
+                                #datadict[param][structured_or_net_name] = torch.cat(images)
                                 
                                 #print('init data struct')
                                 #print(torch.cat(images))
                                 
-                                #datadict[param][structured_or_net_name] = data
+                                datadict[param][structured_or_net_name] = data
                                 
                                 #print('padded data')
                                 #print(data)
