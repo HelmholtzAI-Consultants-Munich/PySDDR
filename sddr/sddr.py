Issue #3 - fixed bug that occurs when datatype int is used instead of float. Also: added explicit error when output of neural networks have wrong shape

sddr/sddr.py
@@ -176,7 +176,7 @@ class Sddr(object):
                 # send each input batch to the current device
                 for param in datadict.keys():
                     for data_part in datadict[param].keys():
-                        datadict[param][data_part] = datadict[param][data_part].float().to(self.device)
+                        datadict[param][data_part] = datadict[param][data_part].to(self.device)
                         
                 # get the network output
                 self.optimizer.zero_grad()
@@ -210,7 +210,7 @@ class Sddr(object):
                     # send each input batch to the current device
                     for param in datadict.keys():
                         for data_part in datadict[param].keys():
-                            datadict[param][data_part] = datadict[param][data_part].float().to(self.device)
+                            datadict[param][data_part] = datadict[param][data_part].to(self.device)
                     _ = self.net(datadict)
                     # compute the loss and add regularization
                     val_batch_loss = torch.mean(self.net.get_log_loss(target))
@@ -560,7 +560,7 @@ class Sddr(object):
         # send each input batch to the current device
         for parameter in datadict.keys():
             for data_part in datadict[parameter].keys():
-                datadict[parameter][data_part] = datadict[parameter][data_part].float().to(self.device)
+                datadict[parameter][data_part] = datadict[parameter][data_part].to(self.device)
                         
         # get the network output
         with torch.no_grad():

sddr/sddrnetwork.py
@@ -49,6 +49,7 @@ class SddrFormulaNet(nn.Module):
         
         super(SddrFormulaNet, self).__init__()
         self.deep_models_dict = deep_models_dict
+        self.deep_shapes = deep_shapes
         
         #register external neural networks
         for key, value in deep_models_dict.items():
@@ -78,6 +79,15 @@ class SddrFormulaNet(nn.Module):
         
         return Utilde
     
+    def _check_network_output_shape(self, Uhat_net, key,datadict):
+        
+        expected_batchsize = datadict[key].shape[0]
+        expetec_output_size = self.deep_shapes[key]
+        
+        actual_output_shape = tuple(Uhat_net.shape)
+        
+        assert actual_output_shape == (expected_batchsize, expetec_output_size), f"Expected output of {key} to be {(expected_batchsize, expetec_output_size)} (batch-size, output_shape), but instead we found {actual_output_shape}"
+    
     
     def forward(self, datadict,training=True):
         X = datadict["structured"]
@@ -87,8 +97,8 @@ class SddrFormulaNet(nn.Module):
             Utilde_list = []
             for key in self.deep_models_dict.keys(): #assume that the input for the NN has the name of the NN as key
                 net = self.deep_models_dict[key]
-                
                 Uhat_net = net(datadict[key])
+                self._check_network_output_shape(Uhat_net, key, datadict)
                 
                 # orthogonalize the output of the neural network with respect to the parts of the structured part,
                 # that contain the same input as the neural network

sddr/utils/prepare_data.py
@@ -4,6 +4,7 @@ import torch
 import pandas as pd
 import os
 import torch.nn as nn
+import numpy as np
 
 class PrepareData(object):
     '''
@@ -215,7 +216,9 @@ class PrepareData(object):
             for net_name in self.formula_terms_dict[param]['net_feature_names'].keys():
                 net_feature_names = self.formula_terms_dict[param]['net_feature_names'][net_name]
                 try:
-                    prepared_data[param][net_name] = torch.from_numpy(data[net_feature_names].to_numpy()).float()
+                    prepared_data[param][net_name] = torch.from_numpy(data[net_feature_names].to_numpy())
+                    if prepared_data[param][net_name].dtype == torch.float64:
+                        prepared_data[param][net_name] = prepared_data[param][net_name].float()
                 except: #if it fails to convert to numpy as the data type is an object: store the data frame itself
                     prepared_data[param][net_name] = data[net_feature_names]
 